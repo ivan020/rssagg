5db472c259fc7e5d0b6558942a5814910298186f
ivan <ivevdm@gmail.com>
2026-03-10 15:37:07 +0000

    started the project development

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main;
 
-import "fmt";
+import (
+	"fmt"
+	"log"
+	"os"
+	"net/http"
+	"github.com/joho/godotenv"
+	"github.com/go-chi/chi"
+	// "github.com/go-chi/cors"
+);
 
 func main() {
-	fmt.Println("hello world");
+	godotenv.Load(".env");
+
+	portString := os.Getenv("PORT");
+	if portString == "" {
+		log.Fatal("PORT is not found in the env");
+	}
+
+	router := chi.NewRouter();
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	};
+	
+	log.Printf("Server starting on port %v", portString);
+	err := srv.ListenAndServe();
+	if err != nil {
+		log.Fatal(err);
+	}
+
+
+	fmt.Println("Port:", portString);
 }
