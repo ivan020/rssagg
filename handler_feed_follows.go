dd89ef3b78c51afca8da7a5db91364263fd38538
ivan <ivevdm@gmail.com>
2026-03-11 15:58:38 +0000

    created the handler to create the feed id

---
+++ handler_feed_follows.go
@@ -0,0 +1,40 @@
+package main;
+
+import (
+ "net/http" 
+ "github.com/google/uuid"
+)
+
+
+func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		FeedID uuid.UUID string `json:"feed_id"`
+
+	}
+	decoder := json.NewDecoder(r.Body);
+	decoder.Decode();
+	
+	params := parameters{};
+	err := decoder.Decode(&params);
+	if err != nil {
+		respondWithError(w, 400,fmt.Sprintf("Error parsing json: %v", err));
+		return;
+	}
+
+	feedFollow, err := apiCfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedA: time.Now().UTC(),
+		UserID: feed.ID,
+		FeedID: params.FeedID,
+	});
+
+	if err != nil {
+		respondWithError(w, 400,fmt.Sprintf("Couldn't create feed follow: %v", err));
+		return;
+	}
+
+	respondWithJson(w, 201, databaseFeedFollowToFeedFollow( feedFollow ));
+
+}
+
