f5b6ec818fcfc4104073a99b40a577cc9e1fa71f
ivan <ivevdm@gmail.com>
2026-03-12 15:15:56 +0000

    created the blog post parsing

--- scraper.go
+++ scraper.go
@@ -51,8 +51,37 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post", item.Title, "on feed", feed.Name);
+			description := sql.NullString{
+				if item.Description != "" {
+					description.String = item.Description
+					description.Valid = true,
+				}
+			}
+
+			pubAt, err := time.Parse(time.RFC1123Z, item.PubDate);
+			if err != nil {
+				log.Println("Couldn't parse date %v with err %v ", item.PubDate, err);
+				continue;
+			}
+
+
+			_, err := db.CreatePost(context.Background(), database.CreatePostParams{
+			ID: uuid.New(),
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+			Title: item.Title,
+			Description: description,
+			PublishedAt: pubAt,
+			Url: item.Link,
+			FeedID: feed.ID,
+		})
+
+		if err != nil {
+			log.Println("failed to create post:", err);
+			continue;
+		}
 	}
+
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item));
 
 }
