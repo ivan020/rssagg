35b3b9cb0941bac99fbe9ff11e2f153649cdb983
ivan <ivevdm@gmail.com>
2026-03-12 14:52:16 +0000

    changed the log message

--- scraper.go
+++ scraper.go
@@ -51,7 +51,9 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post", item.Title);
+		log.Println("Found post", item.Title, "on feed", feed.Name);
 	}
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item));
+
 }
+
